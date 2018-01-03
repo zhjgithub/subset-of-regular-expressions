3ce36b1028ff0196aa0ae758d88b9d19b5b49e76
organism <neuqzhj@live.com>
2018-01-03 15:06:50 +0800

    feature: Add operations

--- subset_re.py
+++ subset_re.py
@@ -4,6 +4,15 @@ Exercise of subset of Regular Expressions implement.
 
 null = frozenset()
 
+def lit(string):  return ('lit', string)
+def seq(x, y):    return ('seq', x, y)
+def alt(x, y):    return ('alt', x, y)
+def star(x):      return ('star', x)
+def plus(x):      return (seq(x, star(x)))
+def opt(x):       return alt(lit(''), x) #opt(x) means that x is optional
+def oneof(chars): return ('oneof', tuple(chars))
+dot = ('dot',)
+eol = ('eol',)
 
 def matchset(pattern, text):
     '''
@@ -39,6 +48,17 @@ def components(pattern):
 
 def test():
     "Tests."
+    assert lit('abc')         == ('lit', 'abc')
+    assert seq(('lit', 'a'), 
+               ('lit', 'b'))  == ('seq', ('lit', 'a'), ('lit', 'b'))
+    assert alt(('lit', 'a'), 
+               ('lit', 'b'))  == ('alt', ('lit', 'a'), ('lit', 'b'))
+    assert star(('lit', 'a')) == ('star', ('lit', 'a'))
+    assert plus(('lit', 'c')) == ('seq', ('lit', 'c'), 
+                                  ('star', ('lit', 'c')))
+    assert opt(('lit', 'x'))  == ('alt', ('lit', ''), ('lit', 'x'))
+    assert oneof('abc')       == ('oneof', ('a', 'b', 'c'))
+
     assert matchset(('lit', 'abc'), 'abcdef') == set(['def'])
     assert matchset(('seq', ('lit', 'hi '), ('lit', 'there ')),
                     'hi there nice to meet you') == set(['nice to meet you'])
